20271ba42d242b31f0aaa7b9b58b71dff31d6e28
PietraSus <1234fffeny@gmail.com>
2024-05-27 20:23:25 +0200

    Jeszcze więcej grafik

---
+++ gam/Assets/ModuleLod.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleLod : MonoBehaviour
+{
+    public GameObject lowQuality;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lowQuality.active = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
